a00a9a788ee10949381d22b8fa0ce46284678ba1
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-05-25 14:03:27 +0200

    Add enviroment variables

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,7 @@
 export async function FetchGet(params) {
-  const API_KEY = "IzbYhIAZuZFbxpTQgB0atvPZNe8s02lxLzXVzCXs";
+  const EXPO_PUBLIC_API_KEY = "IzbYhIAZuZFbxpTQgB0atvPZNe8s02lxLzXVzCXs";
   const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${
+    `https://api.nasa.gov/planetary/apod?api_key=${EXPO_PUBLIC_API_KEY}${
       params?.length > 0 ? params : ""
     }`
   );
